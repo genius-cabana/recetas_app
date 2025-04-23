106ea0879c39bf4742a8dc65d1004d6a018c581c
genius-cabana <1513661@senati.pe>
2025-04-22 21:18:43 -0500

    Mejora de URL2

--- frontend/editar.php
+++ frontend/editar.php
@@ -49,7 +49,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Gestor de Recetas</title>
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="../frontend/css/styles.css">
 </head>
 <body>
     <div class="container">
